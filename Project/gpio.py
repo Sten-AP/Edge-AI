87f0d18f414ada2450296cbd06bf38aa452f8642
Sten Hulsbergen <s122441@ap.be>
2023-12-22 09:26:48 +0100

    Add exception when program stops to turn leds off

--- Project/gpio.py
+++ Project/gpio.py
@@ -5,18 +5,23 @@ led_geel = GPIO("/dev/gpiochip4", 13, "out")  # pin 36
 led_rood = GPIO("/dev/gpiochip2", 13, "out")  # pin 37
 
 def toggle_led(prediction):
-    print(prediction)
-    if prediction == 'aardbei':
-        led_rood.write(True) 
-    if prediction == 'kers':
+    try:
+        if prediction == 'aardbei':
+            led_rood.write(True) 
+        if prediction == 'kers':
+            led_rood.write(False)     
+
+        if prediction == 'boom':
+            led_groen.write(True)     
+        if prediction == 'gras':
+            led_groen.write(False)
+
+        if prediction == 'kaas':
+            led_geel.write(True)     
+        if prediction == 'zon':
+            led_geel.write(False)
+    except KeyboardInterrupt as e:
+        print("Shutdown")
         led_rood.write(False)     
-
-    if prediction == 'boom':
-        led_groen.write(True)     
-    if prediction == 'gras':
         led_groen.write(False)
-
-    if prediction == 'kaas':
-        led_geel.write(True)     
-    if prediction == 'zon':
         led_geel.write(False)
